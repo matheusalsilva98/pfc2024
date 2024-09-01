6210bcc8fc3801b74dd97b9d3e6367120ff5b342
reginaldopsilvaf <126976300+reginaldopsilvaf@users.noreply.github.com>
2024-09-01 16:12:33 -0300

    changed logs saving in disk and tensorboard

--- pfc_unet_cloud_seg/callbacks.py
+++ pfc_unet_cloud_seg/callbacks.py
@@ -67,6 +67,8 @@ class MyPrintingCallback(Callback):
         logger = trainer.logger
         
         for batch in val_dl:
+            if len(saved_paths) >= 90:
+                break
             images, img_paths, masks = batch['image'], batch['image_path'], batch['mask']
             for image, img_path, mask in zip(images, img_paths, masks):
                 folder_path = os.path.dirname(img_path)
